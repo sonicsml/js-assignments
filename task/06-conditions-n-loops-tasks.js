e5b3afe3c6afd37fa7b874946ef1d0e1691d750a
sonicsml <sonics.scs@gmail.com>
2019-04-14 23:01:11 +0300

    fix: fix task 6

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -30,19 +30,19 @@
  *
  */
 function getFizzBuzz(num) {
-        if (i%15 == 0) {
+        if (num%15 == 0) {
             return ('FizzBuzz');
         }
         else
-        if (i%3 == 0) {
+        if (num%3 == 0) {
             return ('Fizz');
         }
         else
-        if (i%5 == 0) {
+        if (num%5 == 0) {
             return ('Buzz');
         }
         else
-            return (i);
+            return (num);
 }
 
 
